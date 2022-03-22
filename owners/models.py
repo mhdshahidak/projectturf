d9f50269b3c1bfc48c1a7b82f1f7340af9c4bed5
shahidbaabtra <mhdshd.ak@gmail.com>
2022-03-23 00:03:30 +0530

    models

--- owners/models.py
+++ owners/models.py
@@ -1,4 +1,8 @@
+import datetime
+from xmlrpc.client import DateTime
 from django.db import models
+
+from app.models import Customer
 
 # Create your models here.
 class Owners(models.Model):
@@ -10,7 +14,7 @@ class Owners(models.Model):
     phone_no = models.CharField(max_length=15)
     booking_no = models.CharField(max_length=15)
     location = models.CharField(max_length=20)
-    status = models.CharField(max_length=10)
+    status = models.CharField(max_length=10 ,default='requested')
     password = models.CharField(max_length=30)
     profile_image = models.ImageField(upload_to='profile/')
     bg_image = models.ImageField(upload_to='background/')
@@ -20,3 +24,24 @@ class Owners(models.Model):
 
     class Meta:
         db_table = 'owners'
+
+class Slote(models.Model):
+    id = models.AutoField(primary_key=True)
+    turf_id = models.ForeignKey(Owners, on_delete=models.CASCADE)
+    date = models.DateField(default=datetime.date.today)
+    slote = models.TimeField()
+    status = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'slote'
+
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)
+    turf_id = models.ForeignKey(Owners, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    slote_id = models.ForeignKey(Slote, on_delete=models.CASCADE)
+    payment = models.FloatField()
+    status = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'booking'
--- owners/views.py
+++ owners/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
 
+from owners.models import Owners
+
 # Create your views here.
 def register(request):
     msg = ""
-    # if request.method == 'POST':
+    if request.method == 'POST':
+      name = request.POST['name']
+      email = request.POST['email']
+      court_name = request.POST['courtname']
+      sport_type = request.POST['sportstype']
+      contactnumber = request.POST['contactnumber']
+      booknumber = request.POST['bookingnumber']
+      location = request.POST['location']
+      password = request.POST['password']
+      cpassword = request.POST['cpassword']
 
-    return render(request,'register.html')
+
+      if password == cpassword:
+          new_owner = Owners(owner_name=name,email=email,court_name=court_name, sports_type=sport_type, phone_no=contactnumber, booking_no=booknumber, location=location,password=password)
+          new_owner.save()
+          msg = "Registerd successfully"
+      else:
+          msg = "Incorrect password "
+
+    return render(request,'register.html', {'msg':msg, })
 
 def owelogin(request):
     return render(request,'owelogin.html')
